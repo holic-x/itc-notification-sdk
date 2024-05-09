c73e760555e34deca1e7e9fc0a5c814d5d8ba4d7
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-05-09 19:22:43 +0800

    feat：新增公告通知弹窗
    新增公告通知弹窗
    以插件形式发布到npm
    调整eslint校验配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,68 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Swal from "sweetalert2";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function alertNotification() {
+    /**
+     * 后端地址（本地）
+     */
+    // const BACKEND_HOST_LOCAL = "http://localhost:8101";
+
+    /**
+     * 后端地址（线上）
+     */
+    // const BACKEND_HOST_PROD = "http://xxx.xxx";
+
+    function getNotificationVoUsingGet(params) {
+
+        const url = `http://localhost:8101/api/admin/cms/notification/getNotificationVOByDomain`;
+     
+        // const url = `${BACKEND_HOST_PROD}/api/notification/get/vo`;
+
+        return fetch(url + "?" + new URLSearchParams(params))
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => data)
+            .catch((error) => {
+                // 处理错误
+                console.error("Fetch request error:", error);
+                throw error;
+            });
+    }
+
+    const fetchNotification = function (domain) {
+        // 发起请求获取通知信息的逻辑
+        getNotificationVoUsingGet({ domain })
+            .then((response) => {
+                const data = response.data;
+                const id = data.id;
+                const updateTime = data.updateTime;
+                if (
+                    !localStorage.getItem(id + updateTime) &&
+                    data.title &&
+                    data.content
+                ) {
+                    // 使用 SweetAlert2 显示弹窗
+                    Swal.fire({
+                        title: data.title,
+                        text: data.content,
+                        icon: "info",
+                        confirmButtonText: "知道了",
+                    });
+
+                    // 存储到 localStorage
+                    localStorage.setItem(id + updateTime, "id");
+                }
+            })
+            .catch((error) => {
+                console.error("Fetch request error:", error);
+            });
+    };
+
+    const url = new URL(location.href);
+    const domain = url.hostname;
+    fetchNotification(domain);
+}
+alertNotification();
